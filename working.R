108a2438c69686549877cccbbe2d56013f7ffdbe
Marcus Leong <marcusleongjx@gmail.com>
2024-04-10 11:13:50 +0800

    Amending data for plot and axes
    Data for plot now runs form 1961 to 2023 and axes are amended. Plot is
    also now a line rather than points due to the large number of points

--- working.R
+++ working.R
@@ -1,9 +1,10 @@
 library(ggplot2)
 setwd("/home/marcus/repos/cpi-singapore")
 data <- read.csv("ConsumerPriceIndexCPI2019AsBaseYearQuarterly.csv")
-genData <- as.data.frame(t(data[1, 2:17]))
+#data for CPI of All Items from 1961 to 2023
+genData <- as.data.frame(t(data[1, 2:253]))
 genData
-i = 16
+i = 252
 add <- c()
 while (i>0) {
   add <- append(add, i)
@@ -11,6 +12,7 @@ while (i>0) {
 }
 genData$QuartersAfter <- add
 colnames(genData)[1] <- 'AllItems'
-genData
-p <- ggplot(data = genData, mapping = aes (x=QuartersAfter, y=AllItems)) + geom_point() + geom_smooth(fill=NA)
+genData$AllItems <- as.numeric(genData$AllItems)
+p <- ggplot(data = genData, mapping = aes (x=QuartersAfter, y=AllItems)) + geom_line() + geom_smooth(method='lm', se=FALSE) + labs(title='Scatterplot of CPI of All Items vs Quarters after 1960', x='Quarters After 1960', y='CPI of All Items') + coord_cartesian(ylim=c(20,120))
 print(p)
+
