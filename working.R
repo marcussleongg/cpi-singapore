c91c39e1280974f4a8ec1431ccb5f8130a56cf54
Marcus Leong <marcusleongjx@gmail.com>
2024-04-10 11:48:45 +0800

    Prepare relevant data for 2014 onwards

--- working.R
+++ working.R
@@ -16,3 +16,32 @@ genData$AllItems <- as.numeric(genData$AllItems)
 p1961 <- ggplot(data = genData, mapping = aes (x=QuartersAfter, y=AllItems)) + geom_line() + labs(title='Scatterplot of CPI of All Items vs Quarters after 1960 (2019 taken as base)', x='Quarters After 1960', y='CPI of All Items') + coord_cartesian(ylim=c(20,120))
 print(p1961)
 
+#data for CPI from 2014 to 2023
+data2014 <- as.data.frame(t(data[c(1,3,67,84,103,113,130), 2:41]))
+data2014
+i = 40
+add2014 <- c()
+while (i>0) {
+  add2014 <- append(add2014, i)
+  i <- i - 1
+}
+add2014
+data2014$QuartersAfter <- add2014
+colnames(data2014)[1] <- 'AllItems'
+data2014$AllItems <- as.numeric(data2014$AllItems)
+colnames(data2014)[2] <- 'FoodExclServices'
+data2014$FoodExclServices <- as.numeric(data2014$FoodExclServices)
+colnames(data2014)[3] <- 'HousingUtilities'
+data2014$HousingUtilities <- as.numeric(data2014$HousingUtilities)
+colnames(data2014)[4] <- 'Healthcare'
+data2014$Healthcare <- as.numeric(data2014$Healthcare)
+colnames(data2014)[5] <- 'PublicTransport'
+data2014$PublicTransport <- as.numeric(data2014$PublicTransport)
+colnames(data2014)[6] <- 'TelcoServices'
+data2014$TelcoServices <- as.numeric(data2014$TelcoServices)
+colnames(data2014)[7] <- 'Education'
+data2014$Education <- as.numeric(data2014$Education)
+data2014
+p2014 <- ggplot(data = data2014, mapping = aes (x=QuartersAfter, y=AllItems)) + geom_line() + labs(title='Scatterplot of CPI of All Items vs Quarters after 2013 (2019 taken as base)', x='Quarters After 2013', y='CPI of All Items') + coord_cartesian(ylim=c(98,116))
+print(p2014)
+
