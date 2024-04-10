b37df68bbd4521125e9df95c7339760cbd00fd72
Marcus Leong <marcusleongjx@gmail.com>
2024-04-10 10:26:57 +0800

    Clean dataframe and set up for graph

--- working.R
+++ working.R
@@ -1,2 +1,16 @@
+library(ggplot2)
 setwd("/home/marcus/repos/cpi-singapore")
 data <- read.csv("ConsumerPriceIndexCPI2019AsBaseYearQuarterly.csv")
+genData <- as.data.frame(t(data[1, 2:17]))
+genData
+i = 16
+add <- c()
+while (i>0) {
+  add <- append(add, i)
+  i <- i - 1
+}
+genData$QuartersAfter <- add
+colnames(genData)[1] <- 'AllItems'
+genData
+p <- ggplot(data = genData, mapping = aes (x=QuartersAfter, y=AllItems)) + geom_point() + geom_smooth(fill=NA)
+print(p)
