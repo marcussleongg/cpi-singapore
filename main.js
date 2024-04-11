feb4c3d16978d76d5816d8a22373a2cca17a0158
Marcus Leong <marcusleongjx@gmail.com>
2024-04-11 12:51:32 +0800

    Add functionality for close button

--- main.js
+++ main.js
@@ -2,51 +2,78 @@ const imgOne = document.querySelector('#imgone');
 const imgOnePopup = document.querySelector('#imgone_popup')
 imgOne.addEventListener('click', () => {
     imgOnePopup.style["display"] = "block";
+    const closeBtn = imgOnePopup.children[0];
+    closeBtn.addEventListener('click', () => {
+        imgOnePopup.style["display"] = "none";
+    })
 })
 
 const imgTwo = document.querySelector('#imgtwo');
 const imgTwoPopup = document.querySelector('#imgtwo_popup')
 imgTwo.addEventListener('click', () => {
     imgTwoPopup.style["display"] = "block";
+    const closeBtn = imgTwoPopup.children[0];
+    closeBtn.addEventListener('click', () => {
+        imgTwoPopup.style["display"] = "none";
+    })
 })
 
 const imgThree = document.querySelector('#imgthree');
 const imgThreePopup = document.querySelector('#imgthree_popup')
 imgThree.addEventListener('click', () => {
     imgThreePopup.style["display"] = "block";
+    const closeBtn = imgThreePopup.children[0];
+    closeBtn.addEventListener('click', () => {
+        imgThreePopup.style["display"] = "none";
+    })
 })
 
 const imgFour = document.querySelector('#imgfour');
 const imgFourPopup = document.querySelector('#imgfour_popup')
 imgFour.addEventListener('click', () => {
     imgFourPopup.style["display"] = "block";
+    const closeBtn = imgFourPopup.children[0];
+    closeBtn.addEventListener('click', () => {
+        imgFourPopup.style["display"] = "none";
+    })
 })
 
 const imgFive = document.querySelector('#imgfive');
 const imgFivePopup = document.querySelector('#imgfive_popup')
 imgFive.addEventListener('click', () => {
     imgFivePopup.style["display"] = "block";
+    const closeBtn = imgFivePopup.children[0];
+    closeBtn.addEventListener('click', () => {
+        imgFivePopup.style["display"] = "none";
+    })
 })
 
 const imgSix = document.querySelector('#imgsix');
 const imgSixPopup = document.querySelector('#imgsix_popup')
 imgSix.addEventListener('click', () => {
     imgSixPopup.style["display"] = "block";
+    const closeBtn = imgSixPopup.children[0];
+    closeBtn.addEventListener('click', () => {
+        imgSixPopup.style["display"] = "none";
+    })
 })
 
 const imgSeven = document.querySelector('#imgseven');
 const imgSevenPopup = document.querySelector('#imgseven_popup')
 imgSeven.addEventListener('click', () => {
     imgSevenPopup.style["display"] = "block";
+    const closeBtn = imgSevenPopup.children[0];
+    closeBtn.addEventListener('click', () => {
+        imgSevenPopup.style["display"] = "none";
+    })
 })
 
 const imgEight = document.querySelector('#imgeight');
 const imgEightPopup = document.querySelector('#imgeight_popup')
 imgEight.addEventListener('click', () => {
     imgEightPopup.style["display"] = "block";
-})
-
-const closeBtn = document.querySelectorAll('#close');
-closeBtn.addEventListener('click', () => {
-    imgOnePopup.style["display"] = "none";
+    const closeBtn = imgEightPopup.children[0];
+    closeBtn.addEventListener('click', () => {
+        imgEightPopup.style["display"] = "none";
+    })
 })
